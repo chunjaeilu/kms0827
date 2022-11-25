7ca5d0c560bc0a6ea17accb5583f8555be1b7434
chunjaeilu <chunjaeilu@naver.com>
2022-11-25 15:09:58 +0900

    이미지슬라이드 추가

--- script/main.js
+++ script/main.js
@@ -1,10 +1,29 @@
 $(function () {
   $("header #gnb .depth1 > li").hover(
+    /* gnb 호버효과 */
     function () {
+      // mouse over
       $(this).children(".depth2").addClass("show");
+      // 전체 보이기
+      // $("header #gnb .depth2").addClass("show");
     },
     function () {
+      // mouse out
       $(this).children(".depth2").removeClass("show");
+      // 전체 가리기
+      // $("header #gnb .depth2").removeClass("show");
     }
   );
+
+  /* 메인비주얼 슬라이드 효과 */
+  let imgNum = 0;
+  setInterval(function () {
+    imgNum++;
+    if (imgNum >= 3) {
+      imgNum = 0;
+    }
+    $("#main-visual .slide-box").css({
+      transform: `translateY(${-(imgNum * 300)}px)`,
+    });
+  }, 3000);
 });
